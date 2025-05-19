e220e6282187db5e22c77fd7127ecf32b6146ae6
ninh24012004 <quangninhnqn24@gamil.com>
2025-05-20 00:09:04 +0700

    Api crawling

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import restaurants, dishes
+from app.api.v1.endpoints import restaurants, dishes, crawl
 
 api_router = APIRouter()
 api_router.include_router(restaurants.router, prefix="/restaurants", tags=["restaurants"])
 api_router.include_router(dishes.router, prefix="/dishes", tags=["dishes"])
+api_router.include_router(crawl.router, prefix="/crawl", tags=["Crawl"])
--- backend/app/services/crawler.py
+++ backend/app/services/crawler.py
@@ -3,7 +3,7 @@ import json
 import time
 import os
 
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
 
 def crawling_all_restaurants():
     all_items = []
